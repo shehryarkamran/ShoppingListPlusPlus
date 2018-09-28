1274a259f7653179bf23e7de3cdb5a0aa1cfca51
Shehryar Kamran <kool_dude1996@live.com>
2018-09-29 00:13:36 +0500

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/udacity/firebase/shoppinglistplusplus/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.udacity.firebase.shoppinglistplusplus;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.udacity.firebase.shoppinglistplusplus";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String UNIQUE_FIREBASE_ROOT_URL = "https://shoppinglistplusplus-53fb8.firebaseio.com";
+}
